f53765b85a14b04308560e911edc69aba656253b
David Samir <david.samir.wd@gmail.com>
2020-07-21 02:58:12 +0200

    set the vars

--- main.js
+++ main.js
@@ -1,65 +1,28 @@
-let res = document.createElement("div"),
-    body = document.createElement("div"),
-    math = document.createElement("div");
-
-res.className = "res";
-body.className = "body";
-math.className = "math";
-
-document.querySelector('.Calc').appendChild(res)
-document.querySelector('.Calc').appendChild(body)
-document.querySelector('.Calc').appendChild(math)
-
-for (var i = 1; i < 10; i++) {
-    let num = document.createElement("div")
-    let name = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
-    num.className = "number " + name[i];
-    num.setAttribute('onClick', "num(this.innerHTML)")
-    num.innerHTML = i;
-    document.querySelector('.body').appendChild(num)
-}
-
-let zero = document.createElement("div")
-zero.className = "number zero"
-zero.setAttribute('onClick', "num(this.innerHTML)")
-zero.innerHTML = "0"
-document.querySelector('.body').appendChild(zero)
-
-let mark = document.createElement("div")
-mark.className = "number mark"
-mark.setAttribute('onClick', "num(this.innerHTML)")
-mark.innerHTML = "."
-document.querySelector('.body').appendChild(mark)
-
-function num(number) {
-    document.querySelector('.res').innerHTML += number;
-}
-
-// set the action btns
+let allVal = null;
+const topOut = $('.top--res');
+const main = $('.main--res');
 
 
+let newVal = null,
+    mainVal = null;
 
-rees = document.createElement("div");
-rees.className = "rees btn";
-rees.innerHTML = "< ";
-document.querySelector('.math').appendChild(rees)
+$(".dat").click(function() {
+    allVal += $(this).attr('value');
+    // console.log(allVal)
+    topOut.html(allVal);
 
-plus = document.createElement("div");
-plus.className = "plus btn";
-plus.innerHTML = "+";
-document.querySelector('.math').appendChild(plus)
+    parseInt(allVal);
 
-minus = document.createElement("div");
-minus.className = "minus btn";
-minus.innerHTML = "-";
-document.querySelector('.math').appendChild(minus)
+});
 
-divied = document.createElement("div");
-divied.className = "divied btn";
-divied.innerHTML = "/";
-document.querySelector('.math').appendChild(divied)
+$(".num").click(function() {
+    mainVal += parseInt($(this).attr('value'));
+    main.html(mainVal);
+})
+$(".muin").click(function() {
+    newVal = parseInt(mainVal);
+    let rese = newVal + mainVal;
+    mainVal = null
+    main.html(rese);
 
-mult = document.createElement("div");
-mult.className = "mult btn";
-mult.innerHTML = "*";
-document.querySelector('.math').appendChild(mult)
+})
