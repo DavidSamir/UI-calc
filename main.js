efd44c09a6034b0428b4a4fb0ca44c8b5bb059d9
David Samir <david.samir.wd@gmail.com>
2020-08-03 22:22:43 +0200

    new method less buggy

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-let allVal = null;
-const topOut = $('.top--res');
-const main = $('.main--res');
+let numOne = ""
+let numTwo = ""
+const topRes = document.querySelector(".top--res")
+const mainRes = document.querySelector(".main--res")
+const timeDiv = document.querySelector(".time")
+
+// get the time 
+var d = new Date("2011-04-20T09:30:51.01");
+
+let time = `${d.getHours()}:${d.getMinutes()}`
+timeDiv.innerHTML = time
+
+const combine = (one, two, obr) => {
+    one = parseInt(one);
+    two = parseInt(two);
+    // obr == "+" && one + two;
+    if (obr == "+") {
+        return one + two
+    }
+    if (obr == "-") {
+        return one - two
+    }
+    if (obr == "*") {
+        return one * two
+    }
+
+}
 
 
-let newVal = null,
-    mainVal = null;
 
 $(".dat").click(function() {
-    allVal += $(this).attr('value');
-    // console.log(allVal)
-    topOut.html(allVal);
+    if ((this.classList.contains('num'))) {
+        numOne = this.innerHTML;
+        console.log(numOne);
+        topRes.innerHTML += numOne;
+        mainRes.innerHTML += numOne;
+    } else {
+        topRes.innerHTML += this.innerHTML;
+        mainRes.innerHTML = "";
+        console.log(this.innerHTML)
+        entry = topRes.innerHTML
+        entryArray = entry.split(this.innerHTML);
+        if (entryArray[1] !== "") {
+            mainRes.innerHTML = combine(entryArray[0], entryArray[1], this.innerHTML)
+            topRes.innerHTML = "";
+        }
+    }
 
-    parseInt(allVal);
 
-});
-
-$(".num").click(function() {
-    mainVal += parseInt($(this).attr('value'));
-    main.html(mainVal);
-})
-$(".muin").click(function() {
-    newVal = parseInt(mainVal);
-    let rese = newVal + mainVal;
-    mainVal = null
-    main.html(rese);
+    console.log(entryArray)
 
 })
